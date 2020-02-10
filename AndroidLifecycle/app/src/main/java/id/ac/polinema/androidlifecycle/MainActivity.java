69e9ab98a8e5f80b5a8fd2fce4c298433476c904
Azzahra Dinda <azzahradindas@gmail.com>
2020-02-10 14:49:32 +0700

    Updaate Commit Message

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +15,36 @@ public class MainActivity extends AppCompatActivity {
 
 	// TODO: tambahkan callback onStart() di sini
 
-
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Toast.makeText(this, "On Start Application", Toast.LENGTH_LONG).show();
+	}
+	
 	// TODO: tambahkan callback onStop() di sini
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Toast.makeText(this, "On Stop Application", Toast.LENGTH_SHORT).show();
+	}
 
-	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+
+	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy(
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Toast.makeText(this, "On Restart Application", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Toast.makeText(this, "On Resume Application", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Toast.makeText(this, "On Pause Application", Toast.LENGTH_SHORT).show();
+	}
 }
